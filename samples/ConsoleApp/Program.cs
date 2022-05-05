73064ff366dd9413dbac165da4a46d524050739e
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2022-05-05 16:49:49 +0100

    Revert unwanted changes

--- samples/ConsoleApp/Program.cs
+++ samples/ConsoleApp/Program.cs
@@ -1,17 +1,3 @@
-﻿namespace WasiConsoleApp
-{
-    public class Program
-    {
-        public static int Main()
-        {
-            int a = 10;
-            a += 10;
-            Console.WriteLine($"a value = {a}");
-            Console.WriteLine($"a value = {a += 10}");
-            Console.WriteLine($"a value = {a += 10}");
-            Console.WriteLine($"a value = {a += 10}");
-            Console.WriteLine($"Hello from .NET at {DateTime.Now.ToLongTimeString()}");
-            return 0;
-        }
-    }
-}
+﻿using System.Runtime.InteropServices;
+
+Console.WriteLine($"Hello, world at {DateTime.Now.ToLongTimeString()} on {RuntimeInformation.OSArchitecture}!");
