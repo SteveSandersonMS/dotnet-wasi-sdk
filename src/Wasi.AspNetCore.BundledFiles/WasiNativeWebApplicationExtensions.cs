e52f2178bcf03fc488548ef7d32d2f1d3989b3bc
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2022-08-01 10:44:25 +0100

    Debugging with Mono Debug VS Code extension (#8)

--- src/Wasi.AspNetCore.BundledFiles/WasiNativeWebApplicationExtensions.cs
+++ src/Wasi.AspNetCore.BundledFiles/WasiNativeWebApplicationExtensions.cs
@@ -1,7 +1,8 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Wasi.AspNetCore.BundledFiles;
 
 namespace Microsoft.AspNetCore.Builder;
@@ -10,20 +11,22 @@ public static class WasiNativeWebApplicationExtensions
 {
     public static IApplicationBuilder UseBundledStaticFiles(this IApplicationBuilder app, StaticFileOptions? options = null)
     {
-        options = options ?? new StaticFileOptions()
+        // Not sure why you'd pass a fileprovider if you're asking to use the bundled files, but just in case,
+        // only use the WasiBundledFileProvider if no other was specified
+        if (options?.FileProvider is null)
         {
-            ContentTypeProvider = new FileExtensionContentTypeProvider(),
-            ServeUnknownFileTypes = true
-        };
-
-        if (options.FileProvider is not null)
-        {
-            throw new ArgumentException("The options must not specify a file provider.");
+            var env = app.ApplicationServices.GetRequiredService<IWebHostEnvironment>();
+            env.WebRootFileProvider = new WasiBundledFileProvider();
         }
 
-        options.FileProvider = new WasiBundledFileProvider();
-
-        app.UseStaticFiles(options);
+        if (options is null)
+        {
+            app.UseStaticFiles();
+        }
+        else
+        {
+            app.UseStaticFiles(options);
+        }
 
         return app;
     }
