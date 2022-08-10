c4d302f381128e8d289bb019d5763dce8914a17e
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2022-08-10 11:39:58 +0100

    Reference WasmAppBuilder tasks as shared source

---
+++ src/Wasi.Sdk/Tasks/AttributeStubs.cs
@@ -0,0 +1,12 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Diagnostics.CodeAnalysis;
+
+// These are used to make the shared sources from WasmAppBuilder compile for .NET 4.7 for VS tasks
+
+#if NET7_0_OR_GREATER
+#else // .NET Framework
+class NotNullAttribute : Attribute { }
+class NotNullWhenAttribute : Attribute { public NotNullWhenAttribute(bool value) {} }
+#endif
